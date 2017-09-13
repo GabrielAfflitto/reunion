36ad30b65fab2a5e8dd16c45da67b4dde4b637d8
Gabe Afflitto <gafflitto92@gmail.com>
2017-09-13 10:00:16 -0600

    Gabe reunion 1708b

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,17 @@
 class Activity
+  attr_reader :name, :participants, :key, :value
+  def initialize(name)
+    @name = name
+    @participants = {}
+    @key = key
+    @value = value
+  end
+
+  def add_participants
+    participants = name
+  end
+
+  def total_cost
+    participants.value
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,34 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("hiking")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_for_name
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end
+
+  def test_for_participants
+    activity = Activity.new("hiking")
+
+    assert_equal ({}), activity.participants
+  end
+
+  def test_can_add_particpants
+    activity = Activity.new("hiking")
+
+    assert_equal ({"gabe" => 40}), activity.add_participants
+  end
+
+  def test_for_total_cost_of_activity
+    activity = Activity.new("hiking")
+    activity.add_participants(key, value)
+
+    assert_equal 40, activity.total_cost
+  end
 end
